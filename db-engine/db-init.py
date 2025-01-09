6f950350bc11c16ba4fbafdb2337cb10dbfabd94
Harshit Timmanagoudar <harshitster@gmail.com>
2025-01-08 18:35:37 -0800

    Formatted db-init.py file using python class

--- db-engine/db-init.py
+++ db-engine/db-init.py
@@ -1,91 +1,113 @@
 import os
-import psycopg2 # type: ignore
+import psycopg2 
 import logging
-from psycopg2.extras import DictCursor # type: ignore
+from psycopg2.extras import DictCursor 
 
-# Set up logging
-dir_path = os.path.join('/app/db-store')
-os.makedirs(dir_path, exist_ok=True)
-logging.basicConfig(
-    filename=os.path.join(dir_path, 'db-init.log'),
-    level=logging.DEBUG,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    filemode='w'
-)
+class DBInit:
+    def __init__(self):
+        self.setup_logging()
 
-# Database connection parameters
-host = os.environ['POSTGRES_HOST']
-port = int(os.environ['POSTGRES_PORT'])
-user = os.environ['POSTGRES_USER']
-password = os.environ['POSTGRES_PASSWORD']
-dbname = os.environ['POSTGRES_DB']
+    def setup_logging(self):
+        dir_path = os.path.join('/app/db-store')
+        os.makedirs(dir_path, exist_ok=True)
+        
+        logging.basicConfig(
+            filename=os.path.join(dir_path, 'db-init.log'),
+            level=logging.DEBUG,
+            format='%(asctime)s - %(levelname)s - %(message)s',
+            filemode='w'
+        )
 
-logging.info(f"Connecting to host: {host}, port: {port}, user: {user}, dbname: {dbname}")
-
-def split_sql_commands(sql_content):
-    commands = []
-    current_command = []
-    in_function = False
-    for line in sql_content.split('\n'):
-        stripped_line = line.strip()
-        if stripped_line.startswith('CREATE OR REPLACE FUNCTION') or stripped_line.startswith('CREATE FUNCTION'):
-            in_function = True
-        current_command.append(line)
-        if in_function:
-            if stripped_line.endswith('LANGUAGE plpgsql;'):
+    def split_sql_commands(self, sql_content):
+        commands = []
+        current_command = []
+        in_function = False
+        in_trigger = False
+        
+        for line in sql_content.split('\n'):
+            stripped_line = line.strip()
+            
+            if stripped_line.startswith('CREATE OR REPLACE FUNCTION') or stripped_line.startswith('CREATE FUNCTION'):
+                in_function = True
+                
+            elif stripped_line.startswith('CREATE EVENT TRIGGER'):
+                in_trigger = True
+                
+            current_command.append(line)
+            
+            if in_function and stripped_line.endswith('LANGUAGE plpgsql;'):
                 commands.append('\n'.join(current_command))
                 current_command = []
                 in_function = False
-        elif stripped_line.endswith(';') and not in_function:
+                
+            elif in_trigger and stripped_line.endswith('notify_schema_change();'):
+                commands.append('\n'.join(current_command))
+                current_command = []
+                in_trigger = False
+                
+            elif not in_function and not in_trigger and stripped_line.endswith(';'):
+                commands.append('\n'.join(current_command))
+                current_command = []
+                
+        if current_command:
             commands.append('\n'.join(current_command))
-            current_command = []
-    if current_command:
-        commands.append('\n'.join(current_command))
-    return commands
+            
+        return commands
+    
+    def execute_sql_file(self, cursor, filename):
+        with open(filename, 'r') as file:
+            content = file.read()
 
-def execute_sql_file(cursor, filename):
-    with open(filename, 'r') as file:
-        content = file.read()
-    logging.debug(f"Content of {filename}:\n{content}")
-    commands = split_sql_commands(content)
-    for command in commands:
-        command = command.strip()
-        if command:
-            logging.debug(f"Executing command:\n{command}")
-            try:
-                cursor.execute(command)
-                logging.info(f"Command executed successfully")
-            except psycopg2.Error as e:
-                logging.error(f"Error executing command: {e}")
-                raise
+        logging.debug(f"Content of {filename}:\n{content}")
+        commands = self.split_sql_commands(content)
 
-try:
-    # Establish database connection
-    conn = psycopg2.connect(
-        host=host,
-        port=port,
-        user=user,
-        password=password,
-        dbname=dbname
-    )
-    logging.info("Connection - Successful")
+        for command in commands:
+            command = command.strip()
+            if command:
+                logging.debug(f"Executing command:\n{command}")
+                try:
+                    cursor.execute(command)
+                    logging.info(f"Command executed successfully")
+                except psycopg2.Error as e:
+                    logging.error(f"Error executing command: {e}")
+                    raise
 
-    with conn.cursor() as cur:
-        # Execute schema_and_triggers.sql
-        logging.info("Executing schema.sql")
-        execute_sql_file(cur, 'schema.sql')
-        logging.info("Schema implementation completed")
+    def run(self):
+        host = os.environ['POSTGRES_HOST']
+        port = int(os.environ['POSTGRES_PORT'])
+        user = os.environ['POSTGRES_USER']
+        password = os.environ['POSTGRES_PASSWORD']
+        dbname = os.environ['POSTGRES_DB']
 
-    conn.commit()
-    logging.info("Database Established Successfully")
+        logging.info(f"Connecting to host: {host}, port: {port}, user: {user}, dbname: {dbname}")
+        try:
+            conn = psycopg2.connect(
+                host=host,
+                port=port,
+                user=user,
+                password=password,
+                dbname=dbname
+            )
+            logging.info("Connection - Successful")
 
-except psycopg2.Error as e:
-    logging.error(f"PostgreSQL error: {e}")
-    if 'conn' in locals():
-        conn.rollback()
-except Exception as e:
-    logging.error(f"Unexpected error: {e}")
-finally:
-    if 'conn' in locals() and conn is not None:
-        conn.close()
-    logging.info("Connection Closed")
+            with conn.cursor() as cur:
+                logging.info("Executing schema.sql")
+                self.execute_sql_file(cur, 'schema.sql')
+                logging.info("Schema implementation completed")
+
+            conn.commit()
+            logging.info("Database Established Successfully")
+        except psycopg2.Error as e:
+            logging.error(f"PostgreSQL error: {e}")
+            if 'conn' in locals():
+                conn.rollback()
+        except Exception as e:
+            logging.error(f"Unexpected error: {e}")
+        finally:
+            if 'conn' in locals() and conn is not None:
+                conn.close()
+            logging.info("Connection Closed")
+
+if __name__ == "__main__":
+    dbinit = DBInit()
+    dbinit.run()
